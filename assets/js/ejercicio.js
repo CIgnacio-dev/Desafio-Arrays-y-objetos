e5806a4f011193824938dc214ca6a83444d7934a
Carlos <60223426+CIgnacio-dev@users.noreply.github.com>
2020-06-01 08:24:03 -0400

    Files

---
+++ assets/js/ejercicio.js
@@ -0,0 +1,86 @@
+/* c=['red', 'green', 'blue', 'yellow'];
+
+
+for (i = 0; i < c.length; i++){
+    text = "<p style='color:" + c[i] + "'> Feliz día de amor " + c[i] + "</p>"
+    document.write(text);
+} */
+/*  =======pedir colores====
+c=prompt("c");
+c=c.split(" ");
+
+for (i = 0; i < c.length; i++){
+    text = "<p style='color:" + c[i] + "'> Feliz día de amor " + "</p>"
+    document.write(text);
+} */
+/* function promedio{
+    
+}
+alumnos= [
+    {nombre:"Javiera ", edad:20},
+    {nombre:"Francisca ", edad:50},
+    {nombre:"Camilo ", edad:4}
+]
+
+
+for(i=0;i<alumnos.length;i++){
+    document.write(('Nombre :')+(alumnos[i].nombre)) + document.write(('Edad :')+alumnos[i].edad+"<br>")
+    document.write()
+} */
+
+/* var series= ["Flash", "Supergirl", "Arrow", "DC"];
+series.length=0
+alert(series.length); */
+
+/* var age= prompt('');
+var isAdult=(age < 18) ? "Too young" : "Old enough";
+document.write(isAdult);
+ */
+
+/*  var edad= prompt('');
+ var imprime=' ';
+
+ switch(edad){
+     case 18:
+         imprime='Acabas de cumplir la mayoria de edad';
+         break;
+    case 25:
+        imprime='Ya eres adulto';
+        break;
+    case 75:
+        imprime='Ya eres adulto mayor';
+        break;
+    default:
+        imprime='Tu edad es neutra';
+        break;
+ } 
+ console.log(imprime); */
+/* var num=100;
+
+ for(i=0;i<=100;i++)
+    console.log(i); */
+
+/* function printTime(){
+    var d=NewDate()
+    var hours=d.getHours()
+    var mins=d.getMins()
+    var secs=d.getSecs();
+    document.body.innerHTML=hours+":"+mins+":"+secs;
+}
+setInterval(printTime,1000); */
+
+/* var year=2019;
+while(year<=2055){
+    console.log('Estamos en el año '+year);
+    if(year==2000){
+        break;
+    }
+    year++
+    
+} */
+/* var year=30;
+do{
+    console.log(year);
+    year--;
+}   while (year > 25)
+ */
